aa766065e91f26d43133adffe9a5293c7cc5f5a4
Oleg Kirpa <profix.ok@gmail.com>
2023-05-20 20:11:22 +0300

    end

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,13 +1,16 @@
-import { useState } from "react";
-import PropTypes from 'prop-types';
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
+import { useDispatch, useSelector } from 'react-redux';
+import { getContacts, addContact } from '../../redux/slice';
 import './ContactForm.module.css';
 
-const ContactForm = ({ onSubmit }) => {
+export const ContactForm = () => {
   const [name, setName] = useState('');
   const [number, setNumber] = useState('');
+  const contacts = useSelector(getContacts);
+  const dispatch = useDispatch();
 
   const handleChange = event => {
-    console.log(event.target.value, event.target.name);
     const { name, value } = event.target;
     switch (name) {
       case 'name':
@@ -23,43 +26,48 @@ const ContactForm = ({ onSubmit }) => {
 
   const handleSubmit = event => {
     event.preventDefault();
-    onSubmit(name, number);
+    const contact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+
+    const enterContacts = contacts.some(
+      contact =>
+        (contact.name === name.toLowerCase() && contact.number === number) ||
+        contact.number === number
+    );
+    enterContacts
+      ? alert(`${name} or ${number} is already in contacts`)
+      : dispatch(addContact(contact));
+
     setName('');
     setNumber('');
   };
 
-    return (
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="name"
-          value={name}
-          onChange={handleChange}
-          placeholder="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-        />
-        <input
-          type="tel"
-          name="number"
-          value={number}
-          onChange={handleChange}
-          placeholder="number"
-          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-          required
-        />
-
-        <button type="submit">Add contact</button>
-      </form>
-    );
-}
-
-ContactForm.prototypes = {
-  name: PropTypes.string.isRequired,
-  number: PropTypes.number.isRequired,
-  onSubmit: PropTypes.func.isRequired,
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="name"
+        value={name}
+        onChange={handleChange}
+        placeholder="Name"
+        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required
+      />
+      <input
+        type="tel"
+        name="number"
+        value={number}
+        onChange={handleChange}
+        placeholder="number"
+        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+        required
+      />
+      <button type="submit">Add contact</button>
+    </form>
+  );
 };
-
-export default ContactForm;
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,38 +1,37 @@
-import PropTypes from 'prop-types';
 import css from './ContactList.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { getContacts, getFilter, deleteContact } from '../../redux/slice';
 
-const ContactList = ({ contacts, deleteContact }) => {
+export const ContactList = () => {
+  const dispatch = useDispatch();
+  const contacts = useSelector(getContacts);
+  const filter = useSelector(getFilter);
+
+  const findContacts = () => {
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+
+  const filteredContacts = findContacts();
+
   return (
-      <ul>
-        {contacts.length !== 0 ? (
-          contacts.map(({ name, id, number }) => (
-            <li key={id}>
-              <p>
-                {name}: {number}
-              </p>
-              <button type="button" onClick={() => deleteContact(id)}>
-                Delete
-              </button>
-            </li>
-          ))
-        ) : (
-            <li className={css.nocontacts}>No contacts found</li>
-        )}
-      </ul>
+    <ul>
+      {contacts.length !== 0 ? (
+        contacts.map(({ name, id, number }) => (
+          <li key={id}>
+            <p>
+              {name}: {number}
+            </p>
+            <button type="button" onClick={() => dispatch(deleteContact(id))}>
+              Delete
+            </button>
+          </li>
+        ))
+      ) : (
+        <li className={css.nocontacts}>No contacts found</li>
+      )}
+    </ul>
   );
-};
-
-ContactList.propTypes = {
-  contacts: PropTypes.arrayOf(
-    PropTypes.exact({
-      id: PropTypes.string.isRequired,
-      name: PropTypes.string.isRequired,
-      number: PropTypes.string.isRequired,
-    })
-  ).isRequired,
-  deleteContact: PropTypes.func.isRequired,
-};
-
-export default ContactList;
-
-  
+}
